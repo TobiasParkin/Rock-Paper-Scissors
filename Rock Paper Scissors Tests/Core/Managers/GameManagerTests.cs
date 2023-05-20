c94d09ba40723582f6de48999f3df0d303f53e82
TobiasParkin <b9018513@my.shu.ac.uk>
2023-05-20 18:13:25 +0100

    ++ Reduced Code by using Theories instead of Fact

--- Rock Paper Scissors Tests/Core/Managers/GameManagerTests.cs
+++ Rock Paper Scissors Tests/Core/Managers/GameManagerTests.cs
@@ -5,50 +5,16 @@
     using Rock_Paper_Scissors.Resources.States;
     public class GameManagerTests
     {
-        [Fact]
-        public void GetRoundOutcomeReturnsTie()
+        [Theory]
+        [InlineData(Moves.Lizard, Moves.Lizard, PostRoundStates.Tie)]
+        [InlineData(Moves.Rock, Moves.Lizard, PostRoundStates.Win)]
+        [InlineData(Moves.Paper, Moves.Lizard, PostRoundStates.Lose)]
+        public void GetRoundOutcomeReturnsTie(Moves _playerOneMoveMoq, Moves _playerTwoMoveMoq,
+            PostRoundStates _expectedResult)
         {
-            var _playerOneMoveMoq = Moves.Lizard;
-
-            var _playerTwoMoveMoq = Moves.Lizard;
-
             GameManager _gameManager = GameManager.Instance;
 
             var _result = _gameManager.GetRoundOutcome(_playerOneMoveMoq, _playerTwoMoveMoq);
-
-            var _expectedResult = PostRoundStates.Tie;
-
-            Assert.Equal(_expectedResult, _result);
-        }
-
-        [Fact]
-        public void GetRoundOutcomeReturnsWin()
-        {
-            var _playerOneMoveMoq = Moves.Rock;
-
-            var _playerTwoMoveMoq = Moves.Lizard;
-
-            GameManager _gameManager = GameManager.Instance;
-
-            var _result = _gameManager.GetRoundOutcome(_playerOneMoveMoq, _playerTwoMoveMoq);
-
-            var _expectedResult = PostRoundStates.Win;
-
-            Assert.Equal(_expectedResult, _result);
-        }
-
-        [Fact]
-        public void GetRoundOutcomeReturnsLose()
-        {
-            var _playerOneMoveMoq = Moves.Paper;
-
-            var _playerTwoMoveMoq = Moves.Lizard;
-
-            GameManager _gameManager = GameManager.Instance;
-
-            var _result = _gameManager.GetRoundOutcome(_playerOneMoveMoq, _playerTwoMoveMoq);
-
-            var _expectedResult = PostRoundStates.Lose;
 
             Assert.Equal(_expectedResult, _result);
         }
--- Rock Paper Scissors Tests/Core/Managers/RulesManagerTests.cs
+++ Rock Paper Scissors Tests/Core/Managers/RulesManagerTests.cs
@@ -3,52 +3,19 @@
     using Rock_Paper_Scissors.Resources.MoveSet;
     using Rock_Paper_Scissors.Core.Managers;
     using Rock_Paper_Scissors.Resources.States;
+
     public class RulesManagerTests
     {
-        [Fact]
-        public void GetMoveOutcomeReturnsTie()
+        [Theory]
+        [InlineData(Moves.Lizard, Moves.Lizard, PostRoundStates.Tie)]
+        [InlineData(Moves.Rock, Moves.Lizard, PostRoundStates.Win)]
+        [InlineData(Moves.Paper, Moves.Lizard, PostRoundStates.Lose)]
+        public void GetMoveOutcomeReturnsTie(Moves _playerOneMoveMoq, Moves _playerTwoMoveMoq,
+            PostRoundStates _expectedResult)
         {
-            var _playerOneMoveMoq = Moves.Lizard;
-
-            var _playerTwoMoveMoq = Moves.Lizard;
-
-RulesManager _rulesManager = RulesManager.Instance;
-
-var _result = _rulesManager.GetMoveOutcome(_playerOneMoveMoq, _playerTwoMoveMoq);
-
-var _expectedResult = PostRoundStates.Tie;
-
-Assert.Equal(_expectedResult, _result);
-        }
-
-        [Fact]
-        public void GetMoveOutcomeReturnsWin()
-        {
-            var _playerOneMoveMoq = Moves.Rock;
-
-            var _playerTwoMoveMoq = Moves.Lizard;
-
             RulesManager _rulesManager = RulesManager.Instance;
 
             var _result = _rulesManager.GetMoveOutcome(_playerOneMoveMoq, _playerTwoMoveMoq);
-
-            var _expectedResult = PostRoundStates.Win;
-
-            Assert.Equal(_expectedResult, _result);
-        }
-
-        [Fact]
-        public void GetMoveOutcomeReturnsLose()
-        {
-            var _playerOneMoveMoq = Moves.Paper;
-
-            var _playerTwoMoveMoq = Moves.Lizard;
-
-            RulesManager _rulesManager = RulesManager.Instance;
-
-            var _result = _rulesManager.GetMoveOutcome(_playerOneMoveMoq, _playerTwoMoveMoq);
-
-            var _expectedResult = PostRoundStates.Lose;
 
             Assert.Equal(_expectedResult, _result);
         }
