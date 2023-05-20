cd9adacbabdfc05ded5496797f250b0dcd5661de
TobiasParkin <b9018513@my.shu.ac.uk>
2023-05-20 18:17:53 +0100

    ++ Reduced code using Theroy

--- Rock Paper Scissors Tests/Queries/GetMostUsedMoveFromListOfMovesTests.cs
+++ Rock Paper Scissors Tests/Queries/GetMostUsedMoveFromListOfMovesTests.cs
@@ -4,30 +4,20 @@ namespace Rock_Paper_Scissors_Tests.Queries
     using Rock_Paper_Scissors.Resources.MoveSet;
     public class GetMostUsedMoveFromListOfMovesTests
     {
-        [Fact]
-        public void ExecuteReturnsNone()
+
+        public static IEnumerable<object[]> Data()
         {
-            var _query = new GetMostUsedMoveFromListOfMoves();
-
-            var _listOfUsedMoves = new List<Moves>() { };
-
-            var _result = _query.Execute(_listOfUsedMoves);
-
-            var _expectedResult = "None";
-
-            Assert.Equal(_expectedResult, _result);
+            yield return new object[] { new List<Moves> {}, "None" };
+            yield return new object[] { new List<Moves> { Moves.Rock, Moves.Rock, Moves.Paper, Moves.Paper, Moves.Lizard, Moves.Rock, Moves.Scissors, Moves.Spock }, "Rock" };
         }
 
-        [Fact]
-        public void ExecuteReturnsRock()
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ExecuteReturnsExpected(List<Moves> _listOfUsedMoves, string _expectedResult)
         {
             var _query = new GetMostUsedMoveFromListOfMoves();
 
-            var _listOfUsedMoves = new List<Moves>() {Moves.Rock, Moves.Rock, Moves.Paper, Moves.Paper, Moves.Lizard, Moves.Rock, Moves.Scissors, Moves.Spock};
-
             var _result = _query.Execute(_listOfUsedMoves);
-
-            var _expectedResult = "Rock";
 
             Assert.Equal(_expectedResult, _result);
         }
